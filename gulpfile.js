b29fdaebeaf714b6fbfdfc44efc97381ee09e462
Artem <artemivasyuk@mail.ru>
2018-05-03 16:20:33 +0300

    картки

--- gulpfile.js
+++ gulpfile.js
@@ -9,32 +9,35 @@ var gulp = require('gulp'),
     postcss = require('gulp-postcss'),   // PostCSS
     watch = require('gulp-watch');
 
+var scriptsSrc = [
+    'js/jquery-3.3.1.min.js',
+    'js/jquery-ui.min.js',
+    'js/jquery.ui.touch-punch.min.js',
+    'js/bootstrap.min.js',
+    'js/moment.min.js',
+    'js/jquery.mask.min.js',
+    'js/jquery.validate.js',
+    'js/script.js'
+];
+
+var stylesSrc = [
+    'css/bootstrap.min.css',
+    'css/jquery-ui.min.css',
+    'css/style.scss'
+];
 
 gulp.task('js:build', function () {
-    gulp.src([
-        'js/jquery-3.3.1.min.js',
-        'js/jquery-ui.min.js',
-        'js/jquery.ui.touch-punch.min.js',
-        'js/bootstrap.min.js',
-        'js/moment.min.js',
-        'js/jquery.mask.min.js',
-        'js/jquery.validate.js',
-        'js/script.js'
-    ])
-    //.pipe(uglify({
+    gulp.src(scriptsSrc)
+    .pipe(uglify({
 
-    //}))
+    }))
         .pipe(concat('app.min.js')) // Объединяет всё в один файл
         .pipe(gulp.dest('js'))
 });
 
 
 gulp.task('style:build', function () {
-    gulp.src([
-        'css/bootstrap.min.css',
-        'css/jquery-ui.min.css',
-        'css/style.scss'
-    ])
+    gulp.src(stylesSrc)
         .pipe(sass().on('error', sass.logError)) // Компилирует SCSS
         .pipe(concat('app.min.css')) // Объединяет всё в один файл
         .pipe(postcss([
@@ -49,5 +52,24 @@ gulp.task('watch', function(){
 });
 
 gulp.task('default', ['js:build', 'style:build'], function () {
+    console.log('Success!');
+});
+
+gulp.task('dev:js:build', function () {
+    gulp.src(scriptsSrc)
+        .pipe(concat('app.min.js')) // Объединяет всё в один файл
+        .pipe(gulp.dest('js'))
+});
+
+
+gulp.task('dev:style:build', function () {
+    gulp.src(stylesSrc)
+        .pipe(sass().on('error', sass.logError)) // Компилирует SCSS
+        .pipe(concat('app.min.css')) // Объединяет всё в один файл
+        .pipe(gulp.dest('css'))
+});
+
+
+gulp.task('dev', ['dev:js:build', 'dev:style:build'], function () {
     console.log('Success!');
 });
